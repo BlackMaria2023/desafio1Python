46cf500713abdea7cc848a7ddc0e59eb405cc498
mariajose <mariajosevargas605@gmail.com>
2024-02-22 16:19:10 -0300

    otros

--- emprendedor2.py
+++ emprendedor2.py
@@ -1,11 +1,10 @@
 precio = float(input("ingrese el precio de suscripcion"))
-
 usuarios_normales = int(input("ingrese el numero de usuarios normales"))
 usuarios_premium = int(input("ingrese el numero de usuarios premium"))
 gastos = float(input("ingrese los gastos totales"))
 
 
 utilidad_usuario_premium = precio * 1.5 * usuarios_premium
-utilidades = (precio  * usuarios_normales) + utilidad_usuario_premium - gastos
+utilidades = (precio * usuarios_normales) + utilidad_usuario_premium - gastos
 
 print(f"las utilidades totales son : {utilidades}") 
